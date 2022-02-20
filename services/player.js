01e0f75c694082b881456b72ac5d8492e2d9c900
emersonv25 <emersondejesussantos@hotmail.com>
2022-02-20 17:00:32 -0300

    crt+z

--- services/player.js
+++ services/player.js
@@ -5,7 +5,6 @@ const {
     createAudioResource,
     AudioPlayerStatus,
     NoSubscriberBehavior,
-    getVoiceConnection
 } = require('@discordjs/voice');
 const ytSearch = require('yt-search');
 const play = require('play-dl')
@@ -29,15 +28,7 @@ module.exports = {
         else if(queueService.finishedQueue(message.guildId, url))
         {
             this.disableMsgPlayer(message.guildId)
-            
-            if(getVoiceConnection(message.guild.id)){
-                connection.destroy()
-                message.channel.send("Lista de reprodução vazia, Saindo...")
-            }
-            else{
-                message.channel.send("Lista de reprodução vazia !")
-            }
-
+            message.channel.send("Lista de reprodução vazia !")
         }
         else{
             try {
