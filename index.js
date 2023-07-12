4b1a0b3b53b2e4cce1b3add8b996d792a1a007b7
emersonv25 <emersondejesussantos@hotmail.com>
2023-07-12 20:23:34 -0300

    add express para enganar o hosting

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const Discord = require("discord.js");
 require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 const token = process.env.TOKEN
 const prefix = process.env.PREFIX
